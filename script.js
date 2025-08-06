fd5fc9678f2181d68020f278d4789e3022815574
Shinnosuke-Nakayama <nakayama.shinnosuke@outlook.jp>
2025-08-07 00:29:59 +0900

    Update script.js

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ function generatePDF() {
 
   const image = new Image();
   image.crossOrigin = "anonymous";
-  image.src = bgImageUrl;
+  image.src = "https://raw.githubusercontent.com/Shinnosuke-Nakayama/print-maker/refs/heads/main/IMG_3991.jpeg";
 
   image.onload = () => {
     const canvases = [];
